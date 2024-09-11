7e6eb507b8f82cc6528cab74ac7aa6b1c64ea2af
thayanealves <thayanemarciano4@gmail.com>
2024-09-11 14:51:31 -0300

    valor do produto

---
+++ atvd07.py
@@ -0,0 +1,6 @@
+# Enunciado: Uma loja aplica um imposto de 12% sobre o valor dos produtos. Crie um programa que receba o preço de um produto e calcule o preço final com o imposto incluído.
+valor1=int(input("adicione o valor do produto"))
+
+valor_total=valor1+(12/100)
+
+print=(f"o valor total é de {valor_total}")
